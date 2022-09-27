cf9b62c8a3ea6a1a493f6c5548c9d8e177dd3139
Andrés Menchaca <andresfullstack@gmail.com>
2022-09-27 20:59:41 -0300

    update - new data file

--- backend/controllers/public.controller.js
+++ backend/controllers/public.controller.js
@@ -1,7 +1,9 @@
+const data = require("../data/data.json");
+
 const publicController = {
   /* EXAMPLE ROUTE */
   example: async (req, res) => {
-    res.status(200).send({ msg: "Example route" });
+    res.status(200).json({ msg: "entró" });
   },
 };
 
